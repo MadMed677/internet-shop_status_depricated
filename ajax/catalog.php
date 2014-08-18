40a4417e6a8ae5e35632f83b96698d7a7775414d
MadMed677 <madmed677@gmail.com>
2014-08-18 15:39:47 +0600

    Nothing

--- ajax/catalog.php
+++ ajax/catalog.php
@@ -6,33 +6,7 @@
 			<h2>Каталог товаров</h2>
 	
 			<div class="catalog-items">
-
-				<!-- <div class="catalog-item">
-					<div class="inner-catalog-img">
-						<img src="images/catalogImg/1.jpg" alt="">
-					</div>
-					<div class="inner-catalog-item">
-						<h4>Lorem ipsum dolor sit amet.</h4>
-						<div>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore eaque nulla aut magni, placeat a similique voluptates iure at odit.</p>
-						</div>
-						<div class="change-goods">
-							<a class="addOne" href="#"><i class="fa fa-minus"></i></a>
-							<input type="text" value="1">
-							<a class="removeOne" href="#"><i class="fa fa-plus"></i></a>
-						</div>
-					</div>
-					<div class="inner-catalog-specifications">
-						<img src="images/icons/fishki/cherry.png" alt="">
-					</div>
-					<div class="inner-catalog-fishki">
-						<img src="images/icons/fishki/strawberry.png" alt="">
-						<img src="images/icons/fishki/banana.png" alt="">
-						<img src="images/icons/fishki/watermelon.png" alt="">
-						<img src="images/icons/fishki/cherry.png" alt="">
-					</div>
-				</div> -->
-			
+				
 			</div>
 		</div>
 
