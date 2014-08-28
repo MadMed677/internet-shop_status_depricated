ae2d25ba3d54eae9aa956fcb5cb928ac0630d85b
MadMed677 <madmed677@gmail.com>
2014-08-29 00:31:42 +0600

    Add double email sending (to user and to me)

--- ajax/php/email_send.php
+++ ajax/php/email_send.php
@@ -12,12 +12,20 @@ function ValidateEmail( $value ) {
 $post = ( !empty( $_POST ) ) ? true : false;
 $authorName = 'Артем Анашев';
 $authorEmail = 'madmed677@gmail.com';
+$siteEmail = 'mmvpunke@gmail.com';
 $authorVk = 'http://vk.com/madmed677';
+
+$vk = '';
 
 if ( $post ) {
 
 	$email = htmlspecialchars( stripslashes( $_POST['email'] ) );
 	$name = htmlspecialchars( stripslashes( $_POST['name'] ) );
+	$phone = htmlspecialchars( stripslashes( $_POST['phone'] ) );
+
+	if ( isset( $_POST['vk'] ) )
+		$vk = htmlspecialchars( stripslashes( $_POST['vk'] ) );
+
 	$total_price = 0;
 
 	$json = json_decode( $_POST['json'] );
@@ -26,6 +34,8 @@ if ( $post ) {
 
 	$subject = 'Ваш заказ на сайте vPunke';
 	$error = '';
+
+	// Email to user
 	$message = "
 		<html>
 			<head>
@@ -38,7 +48,7 @@ if ( $post ) {
 					$message .= 'Привет <span style="color: #22aba6">' . $name . "</span>.<br>";
 					$message .= 'Информация о твоем заказе представлена ниже. Если ты обнаружил ошибку, то сообщи о ней мне в ближайшее время одним из способов:<br>';
 					$message .= '<ul>
-						<li> Email: '. $authorEmail .'</li>
+						<li> Email: '. $siteEmail .'</li>
 						<li> Vk: '. $authorVk .'</li>
 					</ul>';
 
@@ -81,25 +91,92 @@ if ( $post ) {
 
 					$message .= '<div style="text-align: center;">Итоговая цена: <span style="color: #22aba6">'. $total_price .'</span> рублей</div>';
 
-$message .= "</div></body></html>";
+		$message .= "</div></body></html>";
+
+	// Email to me
+	$message2 = "
+		<html>
+			<head>
+				<title>$subject</title>
+				<link href='http://fonts.googleapis.com/css?family=Roboto:300,400' rel='stylesheet' type='text/css'>
+			</head>
+			<body style='font: 300 1em Roboto'>
+				<div class='wrapper' style='max-width: 700px; margin: 0 auto;'>";
+
+					$message2 .= 'Пользователь <span style="color: #22aba6">'.
+										$name
+								.'</span> с телефоном <span style="color: #22aba6">'.
+										$phone
+								.'</span>';
+								
+					if ( strlen($vk) !== 0 )
+						$message2 .= ' ссылка в VK <span style="color: #22aba6">'. $vk .'</span>';
+
+					$message2 .= '.<br>';
+
+					foreach ( $json as $object ) {
+						$message2 .= '<div style="padding-bottom: 10px; border-bottom: 1px solid #22aba6;">';
+						foreach ( $object as $key => $value ) {
+							$message2 .= '<div style="margin-bottom: 10px;">';
+							if ( $key === 'title' ) {
+
+								$first_title = 'Название: ';
+
+							} elseif ( $key === 'img' ) {
+								
+								continue;
+
+							} elseif ( $key === 'firm' ) {
+								
+								$first_title = 'Производитель: ';
+
+							} elseif ( $key === 'count' ) {
+								
+								$first_title = 'Количество: ';
+							
+							} elseif ( $key === 'price' ) {
+							
+								$first_title = 'Цена: ';
+							
+							} elseif ( $key === 'totalPrice' ) {
+							
+								$total_price += $value;
+								continue;
+							
+							}
+							
+							$message2 .= '<div style="clear: both; float: left; width: 45%; margin-right: 5%;">'.$first_title .'</div><div style="float: left; width: 50%;">'.$value.'</div>';
+							$message2 .= '</div>';
+						}
+						$message2 .= '</div>';
+					}
+
+					$message2 .= '<div style="text-align: center;">Итоговая цена: <span style="color: #22aba6; margin-bottom: 20px;">'. $total_price .'</span> рублей</div>';
+
+		$message2 .= "</div></body></html>";
 
 	if ( !ValidateEmail( $email ) ) {
 		$error = 'Your email is wrong!';
 	}
-
-	$email .= ', ' . $authorEmail;
 
 	$message = wordwrap($message, 70);
 
 	if ( !$error ) {
 		$mail = mail( $email, $subject, $message,
 				"From: ".$name." <".$email.">\r\n"
-                ."Reply-To: ".$email."\r\n"
-                ."Content-type: text/html; charset=utf-8 \r\n"
-                ."X-Mailer: PHP/" . phpversion()
+				."Reply-To: ".$email."\r\n"
+				."Content-type: text/html; charset=utf-8 \r\n"
+				."X-Mailer: PHP/" . phpversion()
 		);
 
-		if ( $mail ) echo 'ok';
+		$mail2 = mail( $siteEmail, $subject, $message2,
+				"From: ".$name." <".$email.">\r\n"
+				."Reply-To: ".$email."\r\n"
+				."Content-type: text/html; charset=utf-8 \r\n"
+				."X-Mailer: PHP/" . phpversion()
+		);
+
+		if ( $mail && $mail2 ) echo 'ok';
 	} else {
 		echo '<div class="bg-error">' . $error . '</div>';
 	}
