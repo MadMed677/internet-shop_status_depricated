37878df32b562d407125629086aea6bdd3c9ebc9
MadMed677 <madmed677@gmail.com>
2014-09-08 18:13:03 +0400

    The minification files

--- dist/scripts.js
+++ dist/scripts.js
@@ -4646,6 +4646,12 @@ var wayp = (function() {
 		}, {
 			offset: '40%'
 		});
+
+		$('.contact-details').waypoint( function() {
+			$(this).children('div').addClass('animated fadeInUp');
+		}, {
+			offset: '50%'
+		});
 	}
 
 	return {
