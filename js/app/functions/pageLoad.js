7d4d964af022d78031d99a958e02c5bcdf8a6d40
MadMed677 <madmed677@gmail.com>
2014-10-04 15:54:42 +0400

    Create function pageLoad (load the page, take: speed, data, callback)

--- js/app/functions/pageLoad.js
+++ js/app/functions/pageLoad.js
@@ -3,12 +3,14 @@ define([
     'jquery',
     'app/plugins/bxSlider',
     'app/plugins/waypoint',
+    'mmPlugins'
 
-], function($, bxSlider, waypoint) {
+], function($, bxSlider, waypoint, mmPlugins) {
 
     function pageLoad(page) {
 
-        var html = '';
+        var html = '',
+            $wrap = $('#wrap');
 
         if ( !page ) {
             page = 'layouts/ajax/index.html';
@@ -23,13 +25,23 @@ define([
             dataType: 'html',
             success: function(data) {
                 html = data;
-                $('#wrap')
-                    .html(data)
-                    .removeClass('fadeInUp')
-                    .addClass('fadeInUp');
 
-                bxSlider.initialize();
-                waypoint.initialize();
+                // Если страницы была загружена
+                if ( $wrap.html().trim() === '' ) {
+
+                    $wrap
+                        .addClass('fadeInUp')
+                        .html(data);
+                }
+                // Если был переход по странице
+                else {
+                    $wrap.mmSlider(1000, data);
+                }
+
+                if ( page.indexOf('catalog') !== 0 ) {
+                    bxSlider.initialize();
+                    waypoint.initialize();
+                }
             },
             error: function() {
                 html = '';
