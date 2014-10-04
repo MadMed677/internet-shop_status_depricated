f04d47e32822972ef2b46074dd67a4f8a9dc1ea8
MadMed677 <madmed677@gmail.com>
2014-10-04 14:01:38 +0400

    Add routes

---
+++ js/app/routes/router.js
@@ -0,0 +1,34 @@
+define([
+
+    'backbone'
+
+], function(Backbone) {
+
+    var Router = Backbone.Router.extend({
+
+        routes: {
+            ''       : 'goToIndex',
+            'catalog': 'goToCatalog',
+            'about'  : 'goToAbout'
+        },
+
+        initialize: function(view) {
+            this.appView = view;
+        },
+
+        goToIndex: function() {
+            this.appView.setPage('index');
+        },
+
+        goToCatalog: function() {
+            this.appView.setPage('catalog');
+        },
+
+        goToAbout: function() {
+            this.appView.setPage('about');
+        }
+
+    });
+
+    return Router;
+});
