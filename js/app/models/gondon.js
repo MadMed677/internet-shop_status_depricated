478644cc7e93060fdd3f0da8458ef6aee766ff32
MadMed677 <madmed677@gmail.com>
2014-10-05 12:17:47 +0400

    Create GondonModel

---
+++ js/app/models/gondon.js
@@ -0,0 +1,37 @@
+define([
+
+    'backbone'
+
+], function(Backbone) {
+
+    var GondonModel = Backbone.Model.extend({
+
+        defaults: {
+            title       : 'Нет названия',
+            img         : 'images/noimage.jpg',
+            description : 'Нет описания',
+            firm        : 'Нет фирмы',
+
+            flavor      : [],
+            specific    : [],
+            size        : [],
+
+            count       : 0,
+            price       : 0
+        },
+
+        validate: function( attributes ) {
+            if ( !attributes.title )
+                return 'Введите название товара';
+            if ( attributes.count < 0  )
+                return 'Количество товара должно быть более 0';
+            if ( attributes.count > 50 )
+                return 'Нельзя заказать так много товара';
+            if ( attributes.price < 0 )
+                return 'Цена должна быть положительной';
+        }
+
+    });
+
+    return GondonModel;
+});
