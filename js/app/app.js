b5e38099e7d7a6e2dc364f78f57788ad6848763b
MadMed677 <madmed677@gmail.com>
2014-10-05 23:10:43 +0400

    Create and delete gondonsCollection

--- js/app/app.js
+++ js/app/app.js
@@ -4,26 +4,15 @@ define([
     'backbone',
     'app/routes/router',
     'app/views/app',
-    'app/collections/gondons',
     'bootstrap'
 
-], function($, Backbone, Router, AppView, GondonsCollection) {
+], function($, Backbone, Router, AppView) {
 
     'use strict';
 
     var initialize = function() {
 
         var appView = new AppView();
-
-        var gondonsCollection = new GondonsCollection();
-        gondonsCollection.create({ title: 'Gondon 1' });
-        gondonsCollection.create({ title: 'Gondon 2' });
-        gondonsCollection.create({ title: 'Gondon 3' });
-        gondonsCollection.fetch();
-
-        window.debug = {
-            gondons: gondonsCollection
-        };
 
         var router = new Router(appView); // передать вид главной модели
         Backbone.history.start();
