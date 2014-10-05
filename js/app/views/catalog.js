61ad341fb9d8a4dc71112a15c07e3a27d6ed29bc
MadMed677 <madmed677@gmail.com>
2014-10-06 03:10:08 +0400

    Рендер коллекции происходит проходом цикла и рендера каждой модели в вид (нет нармально ререндера модели, происходит ререндер коллекциии (а не модели) при каком-либо изменении модели)

--- js/app/views/catalog.js
+++ js/app/views/catalog.js
@@ -23,18 +23,32 @@ define([
         template: Templates['gondon'],
 
         initialize: function() {
+            // Рендерим наш вид
+            this.$el.html( this.html );
+
             // Кешируем переменные
             this.$condoms = this.$('#all-condoms');
 
             this.collection = new GondonsCollection();
+            // this.collection.create({ title: 'Презерватив 1' });
             this.listenTo( this.collection, 'change', this.render );
             this.collection.fetch();
+
+            window.debug = {
+                condoms: this.collection
+            };
         },
 
         render: function() {
 
-            var html = this.template();
-            this.$el.html( html );
+            var condomsHtml = [],
+                that = this;
+
+            this.collection.each( function(condom, index) {
+                condomsHtml.push( that.template( condom.toJSON() ) );
+            });
+
+            this.$condoms.html( condomsHtml.join('') );
 
             return this;
         }
