be1148d408e6d638e81602381faa0027061c03e6
MadMed677 <madmed677@gmail.com>
2014-10-09 22:26:27 +0400

    Добавлена инициализация модального окна с товарами, а так же добавлен вид отображения каталога покупок (10%)

--- js/app/views/catalog.js
+++ js/app/views/catalog.js
@@ -4,15 +4,21 @@ define([
     'backbone',
     'app/collections/gondons',
     'app/templates',
-    'app/views/condoms'
+    'app/views/condoms',
+    'app/views/baskets'
 
-], function($, Backbone, GondonsCollection, Templates, GondonsView) {
+], function($, Backbone, GondonsCollection, Templates, GondonsView, BasketsView) {
 
     var CatalogView = Backbone.View.extend({
 
         id: 'condoms-wrapper',
 
         html: [
+            '<nav class="navbar navbar-default navbar-fixed-top">',
+                '<div class="container">',
+                    '<div class="navbar-header" id="basket-view">Hello</div>',
+                '</div>',
+            '</nav>',
             '<div class="margin-top" id="catalog-page">',
                 '<div class="container">',
                     '<h3 class="text-center">Каталог товаров</h3>',
@@ -20,6 +26,21 @@ define([
                 '</div>',
             '</div>'
         ].join(''),
+
+        events: {
+            'click #basket-view': 'basketView'
+        },
+
+        basketView: function(event) {
+            console.log('click on basketView');
+            var modal = new BasketsView({
+                title: 'Корзина товаров 2',
+                id: 'modal-basket-view',
+                collection: this.collection
+            });
+            modal.show();
+
+        },
 
         template: Templates['gondon'],
 
