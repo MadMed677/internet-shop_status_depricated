7bbf343c48b3cc39f1d944f99bd462e9e2e03f09
MadMed677 <madmed677@gmail.com>
2014-10-16 03:02:13 +0400

    Реализована логика изменение общей стоимости товара при нажатии на кнопки

--- js/app/views/basket.js
+++ js/app/views/basket.js
@@ -17,11 +17,14 @@ define([
         },
 
         changeCount: function(event) {
-            var diff = $(event.target).closest('a').data('diff');
-            var count = this.model.get('count');
+            var diff = $(event.target).closest('a').data('diff'),
+                count = this.model.get('count'),
+                totalSumm = 0,
+                price = this.model.get('price');
 
             this.model.save({
-                'count': count + diff
+                'count': count + diff,
+                'total': (count + diff)*price
             }, { validate: true });
 
             event.preventDefault();
